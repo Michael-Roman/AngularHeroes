d3f62f60531b7fe41ef2d075287bcbec9e5c481b
MikeRoman <msr51@uclive.ac.nz>
2017-02-02 16:15:37 +1300

    Pre-routing commit.

---
+++ app/hero.service.ts
@@ -0,0 +1,14 @@
+/**
+ * Created by Michael on 2/2/17.
+ */
+import {Injectable} from '@angular/core';
+
+import {Hero} from './hero';
+import {HEROES} from './mock-heroes';
+
+@Injectable()
+export class HeroService {
+  getHeroes(): Promise<Hero[]> {
+    return Promise.resolve(HEROES);
+  }
+}
---
+++ app/hero.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by Michael on 2/2/17.
+ */
+export class Hero {
+  id: number;
+  name: string;
+}
