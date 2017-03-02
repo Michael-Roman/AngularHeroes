995e75adfa7e0def8dc06b38d2858e5be5f276a7
MikeRoman <msr51@uclive.ac.nz>
2017-03-02 18:41:17 +1300

    Routing problems fixed. Index html did have a base href.
    Completeted routing and modularised the application.

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -1,25 +1,42 @@
 /**
  * Created by Michael on 2/2/17.
  */
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {ActivatedRoute, Params} from '@angular/router';
+import {Location} from '@angular/common';
+
+import {HeroService} from './hero.service';
+// To use with route params Observable
+import 'rxjs/add/operator/switchMap';
 import {Hero} from "./hero";
 
 @Component({
+  moduleId: module.id,
   selector: 'my-hero-detail',
-  template: `
-    <div *ngIf="hero">
-      <h2>{{hero.name}} details!</h2>
-      <div>
-        <label>id: </label>{{hero.id}}
-      </div>
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="hero.name" placeholder="name">
-      </div>
-    </div>`
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css']
 })
 
-export class HeroDetailComponent {
-  @Input()
-  hero: Hero;
+export class HeroDetailComponent implements OnInit{
+
+  constructor(
+    private heroService: HeroService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) {}
+
+  ngOnInit(): void {
+    // switchMap maps id in observable route parameters to new Observable
+    this.route.params
+      .switchMap((params: Params) =>
+        this.heroService.getHero(+params['id'])).subscribe(hero => this.hero = hero); // + coverts int (JS)
+  }
+
+  goBack(): void {
+    // Careful not to go back too far. Guard with CanDeactivate guard.
+    this.location.back();
+  }
+
+  @Input() hero: Hero;
+
 }
--- app/hero.service.ts
+++ app/hero.service.ts
@@ -8,7 +8,12 @@ import {HEROES} from './mock-heroes';
 
 @Injectable()
 export class HeroService {
+
   getHeroes(): Promise<Hero[]> {
     return Promise.resolve(HEROES);
+  }
+
+  getHero(id: Number): Promise<Hero> {
+    return this.getHeroes().then(heroes => heroes.find(hero => hero.id === id));
   }
 }
